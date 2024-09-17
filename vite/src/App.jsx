8c2bdb550cca9c587dba23ee1c0ea30c1e82edb9
M-Mahad-Siddiqui <mahadsiddiqui21@gmail.com>
2024-09-17 18:05:58 +0500

    add I-Phone App

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,21 +1,54 @@
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './component/Home';
-import NavBar from './component/Navbar';
-import Product from './component/Product';
-import Products from './component/Products';
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import './App.css';
+// import Home from './component/Home';
+// import NavBar from './component/Navbar';
+// import Product from './component/Product';
+// import Products from './component/Products';
 
+
+// function App() {
+//   return (
+//     <Router>
+//       <NavBar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/product/:id" element={<Product />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import About from './components/About';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Product from './components/Product';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
     <Router>
-      <NavBar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/product/:id" element={<Product />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -1,19 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css'
-import App from './App.jsx'
-import './index.css'
-import store from './redux/store'
+import 'font-awesome/css/font-awesome.min.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import App from './App';
+import './index.css';
+import store from './redux/store';
+        // import reportWebVitals from './reportWebVitals';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store = {store}>
-      
-    <App />
+    <Provider store={store}>
+
+      <App />
     </Provider>
   </StrictMode>
 )
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
